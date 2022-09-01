36067a43ed5f8d47d8a385c750fba029d70b4ddf
basicomCsharp <coolandrey@gmail.com>
2022-09-01 21:45:35 +0500

    Task1 for modul 8

---
+++ ConsoleApp2/Cleaner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Cleaner
+    {
+        public static string DirName { get; set; }
+        private static TimeSpan ts;
+        private void DelFile(string dir)
+        {
+            string[] files = Directory.GetFiles(dir);
+            foreach (string s in files)
+            {
+                var fileInfo = new FileInfo(s);
+                ts = DateTime.Now - fileInfo.LastWriteTime;
+                if (ts.Minutes > 30)
+                    fileInfo.Delete();
+            }
+        }
+        public void DelOldFiles(string dir)
+        {
+            DirName = dir;
+            if (Directory.Exists(DirName))
+            {
+                DelFile(DirName);
+                string[] dirs = Directory.GetDirectories(DirName);
+                foreach (string d in dirs)
+                {
+                    DelFile(d);
+                    DelOldFiles(d);
+                }                   
+            }
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)            
         {
+            string sDir ="D:\\Skillfactory\\Del\\";
+            Cleaner clean = new Cleaner();
+            clean.DelOldFiles(sDir);
+
+
         }
     }
 }
